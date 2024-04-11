ff69123dcced93785b423b594638bfb5e5f751ef
Mateus Borges <borgesm.work@gmail.com>
2024-04-11 17:59:54 -0300

    Configuration adjustments and file reorganization

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,22 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.strictTypeChecked,
-  ...tseslint.configs.strictTypeChecked,
+  pluginJs.configs.strict,
+  ...tseslint.configs.strict,
   pluginReactConfig,
   {
+    parser: '@typescript-eslint/parser',
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'warn',
+      'no-console': 'warn',
+      'arrow-spacing': ['error', { before: true, after: true }],
+      singleQuote: true,
+      semi: ['error', 'always'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
-]
+];
