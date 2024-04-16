9f92ec17e872d6a79012f0b9fd958e359260a897
Mateus Borges <borgesm.work@gmail.com>
2024-04-16 09:25:29 -0300

    feat: color constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const mainColor: string = '#126BA5';
+export const accentColor: string = '#52B6FF';
+export const backgroundColor: string = '#FFFFFF';
+
+export const darkMainColor: string = '#1f1f1f';
+export const darkAccentColor: string = '#404040';
+export const darkBackgroundColor: string = '#343434';
+
+export const fontColor: string = '#126BA5';
+export const fontSecondayColor: string = '#666666';
+export const fontAccentColor: string = '#52B6FF';
